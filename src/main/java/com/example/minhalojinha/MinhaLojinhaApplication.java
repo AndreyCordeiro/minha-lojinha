9070617a0fde19dc348249c3e8833b97adebd26e
Andrey <andreybr2002@hotmail.com>
2022-07-04 20:15:42 -0300

    Initial commit

---
+++ src/main/java/com/example/minhalojinha/MinhaLojinhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.minhalojinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaLojinhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinhaLojinhaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minhalojinha/MinhaLojinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minhalojinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaLojinhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
