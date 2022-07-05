31e19dd9dc4165f49e0af7e218c6f9b79ee07b2e
Andrey <andreybr2002@hotmail.com>
2022-07-05 12:39:01 -0300

    Implementado os repositories de funcionário

---
+++ src/main/java/com/example/minhalojinha/repository/CargoFuncionarioRepository.java
@@ -0,0 +1,7 @@
+package com.example.minhalojinha.repository;
+
+import com.example.minhalojinha.model.entity.CargoFuncionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CargoFuncionarioRepository extends JpaRepository<CargoFuncionario, Long> {
+}
